a24da77177f62e33131c57851b66e543d430430e
zhuher <societyofbruh@gmail.com>
2022-11-12 02:04:57 +0300

    Initial commit

---
+++ BitOp.cs
@@ -0,0 +1,303 @@
+namespace ZhukoWaspHwFirst;
+
+/// <summary>
+///     Methods of class <c>BitOp</c> perform bitwise operations with a given number.
+/// </summary>
+public static class BitOp
+{
+    public static Func<long, long> AbsL = x => (+1 | (x >> (sizeof(long) * 7 - 1))) * x;
+
+    public static Func<int, int> AbsI = x => (+1 | (x >> (sizeof(int) * 7 - 1))) * x;
+
+    public static Func<short, short, short, short, long> PackShorts = (first, second, third, fourth) => (ushort)first | ((long)(ushort)second << 16) | ((long)(ushort)third << 32) | ((long)(ushort)fourth << 48);
+    //public static int ShiftRight
+    //public static ulong PackShorts(short first, short second, ushort third, short fourth) => ;
+
+    /// <summary>
+    ///     Extension method <c>BitState</c> checks the state of the given byte's bit at the specified position.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// byte exampleByte = 15; // == ..1111
+    /// bool exampleBool = BitOp.BitState(exampleByte, 0); // least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being true.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// byte exampleByte = 11; // == ..1011
+    /// bool exampleBool = BitOp.BitState(exampleByte, 2); // third least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being false.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Byte to check.</param>
+    /// <param name="bitPosition">Position of the bit to check.</param>
+    /// <returns>State of the byte's bit at the given position.</returns>
+    public static bool BitState(this byte number, byte bitPosition)
+    {
+        if (bitPosition > 7)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 7].");
+        return Convert.ToBoolean(number & (1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>BitState</c> checks the state of the given ushort's bit at the specified position.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// ushort exampleUshort = 15; // == ..1111
+    /// bool exampleBool = BitOp.BitState(exampleUshort, 0); // least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being true.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// ushort exampleUshort = 11; // == ..1011
+    /// bool exampleBool = BitOp.BitState(exampleUshort, 2); // third least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being false.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Ushort to check.</param>
+    /// <param name="bitPosition">Position of the bit to check.</param>
+    /// <returns>State of the ushort's bit at the given position.</returns>
+    public static bool BitState(this ushort number, byte bitPosition)
+    {
+        if (bitPosition > 15)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 15].");
+        return Convert.ToBoolean(number & (1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>BitState</c> checks the state of the given uint's bit at the specified position.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// uint exampleUint = 15; // == ..1111
+    /// bool exampleBool = BitOp.BitState(exampleUint, 0); // least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being true.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// uint exampleUint = 11; // == ..1011
+    /// bool exampleBool = BitOp.BitState(exampleUint, 2); // third least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being false.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Uint to check.</param>
+    /// <param name="bitPosition">Position of the bit to check.</param>
+    /// <returns>State of the uint's bit at the given position.</returns>
+    public static bool BitState(this uint number, byte bitPosition)
+    {
+        if (bitPosition > 31)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 31].");
+        return Convert.ToBoolean(number & (1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>BitState</c> checks the state of the given ulong's bit at the specified position.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// ulong exampleUlong = 15; // == ..1111
+    /// bool exampleBool = BitOp.BitState(exampleUlong, 0); // least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being true.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// ulong exampleUlong = 11; // == ..1011
+    /// bool exampleBool = BitOp.BitState(exampleUlong, 2); // third least significant bit checked
+    /// ================
+    /// </code>
+    ///         results in <c>exampleBool</c>'s value being false.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Ulong to check.</param>
+    /// <param name="bitPosition">Position of the bit to check.</param>
+    /// <returns>State of the ulong's bit at the given position.</returns>
+    public static bool BitState(this ulong number, byte bitPosition)
+    {
+        if (bitPosition > 63)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 63].");
+        return Convert.ToBoolean(number & (ulong)(1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>Set</c> permutes the given byte by changing the state of the bit at the given position, if
+    ///     needed.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// byte exampleByte = 15; // == ..1111
+    /// exampleByte = BitOp.Set(exampleByte, 0, 0); // sets least significant bit to 0
+    /// ================
+    /// </code>
+    ///         results in <c>exampleByte</c>'s having the value of 14.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// byte exampleByte = 15; // == ..1111
+    /// exampleByte = BitOp.Set(exampleByte, 0, 1); // least significant bit is already 0, nothing happens
+    /// ================
+    /// </code>
+    ///         results in <c>exampleByte</c>'s having the value of 15, changing nothing.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Byte to permute.</param>
+    /// <param name="bitPosition">Position of the bit. [0; 31]</param>
+    /// <param name="bitState">State of the bit. [0; 1]</param>
+    /// <returns>Permuted byte.</returns>
+    public static byte Set(this byte number, byte bitPosition, byte bitState)
+    {
+        if (bitState > 1)
+            throw new ArgumentException($"Entered bitState value {bitState} is out of allowed bounds [0; 1].");
+        if (bitPosition > 7)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 7].");
+        return (byte)(Convert.ToBoolean(bitState) == BitState(number, bitPosition)
+            ? number
+            : bitState == 1
+                ? number | (1 << bitPosition)
+                : number & ~(1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>Set</c> permutes the given ushort by changing the state of the bit at the given position, if
+    ///     needed.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// ushort exampleUshort = 15; // == ..1111
+    /// exampleUshort = BitOp.Set(exampleUshort, 0, 0); // sets least significant bit to 0
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUshort</c>'s having the value of 14.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// ushort exampleUshort = 15; // == ..1111
+    /// exampleUshort = BitOp.Set(exampleUshort, 0, 1); // least significant bit is already 0, nothing happens
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUshort</c>'s having the value of 15, changing nothing.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Ushort to permute.</param>
+    /// <param name="bitPosition">Position of the bit. [0; 31]</param>
+    /// <param name="bitState">State of the bit. [0; 1]</param>
+    /// <returns>Permuted ushort.</returns>
+    public static ushort Set(this ushort number, byte bitPosition, byte bitState)
+    {
+        if (bitState > 1)
+            throw new ArgumentException($"Entered bitState value {bitState} is out of allowed bounds [0; 1].");
+        if (bitPosition > 15)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 15].");
+        return (ushort)(Convert.ToBoolean(bitState) == BitState(number, bitPosition)
+            ? number
+            : bitState == 1
+                ? number | (1 << bitPosition)
+                : number & ~(1 << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>Set</c> permutes the given uint by changing the state of the bit at the given position, if
+    ///     needed.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// uint exampleUint = 15; // == ..1111
+    /// exampleUint = BitOp.Set(exampleUint, 0, 0); // sets least significant bit to 0
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUint</c>'s having the value of 14.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// uint exampleUint = 15; // == ..1111
+    /// exampleUint = BitOp.Set(exampleUint, 0, 1); // least significant bit is already 0, nothing happens
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUint</c>'s having the value of 15, changing nothing.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Uint to permute.</param>
+    /// <param name="bitPosition">Position of the bit. [0; 31]</param>
+    /// <param name="bitState">State of the bit. [0; 1]</param>
+    /// <returns>Permuted uint.</returns>
+    public static uint Set(this uint number, byte bitPosition, byte bitState)
+    {
+        if (bitState > 1)
+            throw new ArgumentException($"Entered bitState value {bitState} is out of allowed bounds [0; 1].");
+        if (bitPosition > 31)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 31].");
+        return Convert.ToBoolean(bitState) == BitState(number, bitPosition)
+            ? number
+            : (uint)(bitState == 1
+                ? number | (uint)(1 << bitPosition)
+                : number & ~(1      << bitPosition));
+    }
+
+    /// <summary>
+    ///     Extension method <c>Set</c> permutes the given ulong by changing the state of the bit at the given position, if
+    ///     needed.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// ulong exampleUlong = 15; // == ..1111
+    /// exampleUlong = BitOp.Set(exampleUlong, 0, 0); // sets least significant bit to 0
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUlong</c>'s having the value of 14.
+    ///         <code></code>
+    ///         #2: executing
+    ///         <code>
+    /// ================
+    /// ulong exampleUlong = 15; // == ..1111
+    /// exampleUlong = BitOp.Set(exampleUlong, 0, 1); // least significant bit is already 0, nothing happens
+    /// ================
+    /// </code>
+    ///         results in <c>exampleUlong</c>'s having the value of 15, changing nothing.
+    ///     </example>
+    /// </summary>
+    /// <param name="number">Ulong to permute.</param>
+    /// <param name="bitPosition">Position of the bit. [0; 31]</param>
+    /// <param name="bitState">State of the bit. [0; 1]</param>
+    /// <returns>Permuted ulong.</returns>
+    public static ulong Set(this ulong number, byte bitPosition, byte bitState)
+    {
+        if (bitState > 1)
+            throw new ArgumentException($"Entered bitState value {bitState} is out of allowed bounds [0; 1].");
+        if (bitPosition > 63)
+            throw new ArgumentException($"Entered bitPosition value {bitPosition} is out of allowed bounds [0; 63].");
+        return Convert.ToBoolean(bitState) == BitState(number, bitPosition)
+            ? number
+            : bitState == 1
+                ? number | (ulong)(1 << bitPosition)
+                : number & (ulong)~(1 << bitPosition);
+    }
+}
---
+++ Helper.cs
@@ -0,0 +1,240 @@
+using System.Text;
+
+namespace ZhukoWaspHwFirst;
+
+public static class Helper
+{
+    private const          char   EscapeCharacter = (char)0x1B;
+    public static readonly string ResetFormatting = $"{EscapeCharacter}[0m";
+    public static readonly string EraseLine       = $"{EscapeCharacter}[2K";
+
+    public static string EraseScreenUp(short amount)
+    {
+        string result = EraseLine;
+        while (amount != 0)
+        {
+            result += $"{EscapeCharacter}[{1}F" + EraseLine;
+            --amount;
+        }
+
+        return result;
+    }
+
+    public static short[] UnpackShorts(this string packed)
+    {
+        bool negative = packed[0] == '-';
+        packed = negative
+            ? $"{'0'.Multiply(64 - packed.Substring(1).Length)}{packed.Substring(1)}".ReverseString()
+            : $"{'0'.Multiply(64 - packed.Length)}{packed}".ReverseString();
+        return new[]
+        {
+            (short)packed[..16].ReverseString().BasesAnyTo10(2), (short)packed[16..32].ReverseString().BasesAnyTo10(2), (short)packed[32..48].ReverseString().BasesAnyTo10(2), (short)(negative
+                ? (short)packed[48..].ReverseString().BasesAnyTo10(2) | unchecked((short)(1 << 15))
+                : (short)packed[48..].ReverseString().BasesAnyTo10(2))
+        };
+    }
+
+    public static string ReverseString(this string original)
+    {
+        char[] chars = original.ToCharArray();
+        Array.Reverse(chars);
+        return new String(chars);
+    }
+
+    public static int Max(int a, int b) => a > b
+        ? a
+        : b;
+
+    public static int Min(int a, int b) => a > b
+        ? b
+        : a;
+
+    public static long ToPowerOf(this int original, int power)
+    {
+        if (power == 0) return 1;
+        long result                            = original;
+        for (int i = 1; i < power; ++i) result *= original;
+
+        return result;
+    }
+
+    public static int DigitValue(this char chr) => char.IsDigit(chr)
+        ? chr - 48
+        : char.IsUpper(chr)
+            ? chr - 55
+            : char.IsLower(chr)
+                ? chr - 87
+                : 0;
+
+    public static char ValueToDigit(this byte value) => (char)(value <= 9
+        ? 48 + value
+        : 55 + value);
+
+    public static char ValueToDigit(this short value) => (char)(value <= 9
+        ? 48 + value
+        : 55 + value);
+
+    public static char ValueToDigit(this int value) => (char)(value <= 9
+        ? 48 + value
+        : 55 + value);
+
+    public static char ValueToDigit(this long value) => (char)(value <= 9
+        ? 48 + value
+        : 55 + value);
+
+    public static long GetBiggestPascalTriangleNumber(int row)
+    {
+        long divisor = 1, dividend = 1;
+        for (int i = 1; i <= row / 2; ++i)
+        {
+            divisor  *= i;
+            dividend *= row - i + 1;
+        }
+
+        return dividend / divisor;
+    }
+
+    public static long BasesAnyTo10(this string num, int baseFrom, bool upperCase = true)
+    {
+        int  power    = 0, errorBase = 0;
+        long result   = 0;
+        bool negative = false;
+        if (num[0] == '-')
+        {
+            num      = num.Substring(1);
+            negative = true;
+        }
+
+        foreach (char digit in num)
+            errorBase = digit.DigitValue() >= baseFrom
+                ? Max(digit.DigitValue(), errorBase)
+                : errorBase;
+
+        if (errorBase > 0)
+            throw new ArgumentException(
+                $"'{nameof(baseFrom)}' value entered is too low for the number entered({num}). Should be no less than 2 and at least {errorBase + 1}.");
+        foreach (char digit in num.Reverse())
+        {
+            result += digit.DigitValue() * baseFrom.ToPowerOf(power);
+            ++power;
+        }
+
+        return negative
+            ? result | long.MinValue
+            : result;
+    }
+
+    public static string Base10ToAny(this long num, int toBase)
+    {
+        string result   = "";
+        bool   negative = false;
+        if (toBase is < 2 or > 36)
+            throw new ArgumentException(
+                $"{nameof(toBase)}({toBase}) is out of allowed bounds [2; 36].");
+        if ($"{num}"[0] == '-') negative = true;
+        if (negative)
+        {
+            ulong unum = Convert.ToUInt64($"{num}".Substring(1));
+            while (unum != 0)
+            {
+                result =  ((byte)(unum % (ulong)toBase)).ValueToDigit() + result;
+                unum    /= (ulong)toBase;
+            }
+        } else
+        {
+            while (num != 0)
+            {
+                result =  (num % toBase).ValueToDigit() + result;
+                num    /= toBase;
+            }
+        }
+
+        return negative
+            ? '-' + result
+            : result;
+    }
+
+    public static string Base10ToAny(this ulong num, int toBase)
+    {
+        string result = "";
+        if (toBase is < 2 or > 36)
+            throw new ArgumentException(
+                $"{nameof(toBase)}({toBase}) is out of allowed bounds [2; 36].");
+        while (num != 0)
+        {
+            result =  ((byte)(num % (ulong)toBase)).ValueToDigit() + result;
+            num    /= (ulong)toBase;
+        }
+
+        return result;
+    }
+
+    /*
+     * TODO: Checking alphanumeric input for specified rules
+     */
+    /// <summary>
+    ///     String extension method <c>Multiply</c> returns passed string, multiplied given amount of times.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// string exampleString = "AB0BA";
+    /// exampleString = exampleString.Multiply(2);
+    /// ================
+    /// </code>
+    ///         results in <c>exampleString</c>'s having the value of "AB0BAAB0BA".
+    ///     </example>
+    /// </summary>
+    /// <param name="original">String to multiply</param>
+    /// <param name="multiplier">Multiplier</param>
+    /// <returns>Multiplied string</returns>
+    public static string Multiply(this string original, int multiplier) =>
+        multiplier < 0
+            ? original
+            : new StringBuilder(original.Length * multiplier).Insert(0, original, multiplier).ToString();
+
+    /// <summary>
+    ///     Char extension method <c>Multiply</c> returns string containing passed char, multiplied given amount of times.
+    ///     <example>
+    ///         #1: executing
+    ///         <code>
+    /// ================
+    /// string exampleString = "AB0BA";
+    /// exampleString = 'E'.Multiply(10);
+    /// ================
+    /// </code>
+    ///         results in <c>exampleString</c>'s having the value of "EEEEEEEEEE".
+    ///     </example>
+    /// </summary>
+    /// <param name="original">Char to multiply</param>
+    /// <param name="multiplier">Multiplier</param>
+    /// <returns>Multiplied char in a string</returns>
+    public static string Multiply(this char original, int multiplier) => multiplier < 0
+        ? $"{original}"
+        : new(original, BitOp.AbsI(multiplier));
+
+    public static string GoToLineChar(int line, int chr) => new($"{EscapeCharacter}[{line};{chr}H");
+
+    public static string FormMenu(this string[][] menuItems, int line = 1, int chr = 1)
+    {
+        //╝╗╔╚╣╩╦╠═║╬
+        //menuItems[n].Max(element => element.Length);
+        string returnString = "";
+        int horizontalIndent = 0,
+            maxColumnLength  = menuItems.Max(static item => item.Length);
+        for (int i = 0; i < menuItems.Length; ++i)
+        {
+            int columnWidth    = menuItems[i].Max(static element => element.Length),
+                verticalIndent = 0;
+            for (int j = 0; j < maxColumnLength; ++j)
+            {
+                returnString   += $"{GoToLineChar(line + verticalIndent, chr + horizontalIndent)}{(i == 0 ? (j == 0 ? "╔" : "╠") : (j == 0 ? "╦" : "╬"))}{"═".Multiply(columnWidth)}{(j == 0 ? "╗" : "╣").Multiply(i == menuItems.Length - 1 ? 1 : 0)}{GoToLineChar(line + verticalIndent + 1, chr + horizontalIndent)}║{(j >= menuItems[i].Length ? " ".Multiply(columnWidth) : menuItems[i][j] + " ".Multiply(columnWidth - menuItems[i][j].Length))}{"║".Multiply(i == menuItems.Length - 1 ? 1 : 0)}{(GoToLineChar(line + verticalIndent + 2, chr + horizontalIndent))}{(i == 0 ? "╚" : "╩")}{"═".Multiply(columnWidth)}{"╝".Multiply(i == menuItems.Length - 1 ? 1 : 0).Multiply(j == maxColumnLength - 1 ? 1 : 0)}";
+                verticalIndent += 2;
+            }
+
+            horizontalIndent += columnWidth + 1;
+        }
+
+        return returnString;
+    }
+}
